d24899999506ad7e5785b3651dec7ad2a3cedc76
bupd <bupdprasanth@gmail.com>
2024-09-05 02:32:41 +0530

    recommit everything
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Full Stack Creator Masterclass';
+export const SITE_DESCRIPTION = 'Start Your Journey in Content Creation';
